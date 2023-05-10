5f5de7ca69101cad3af601ee020179001a9606a6
thutqv@fireapps.vn <admin23@24>
2023-05-10 16:26:20 +0700

    Update

--- src/core/queue/manager.ts
+++ src/core/queue/manager.ts
@@ -1,6 +1,7 @@
 import Queue from 'bull';
 import { redisConfig } from '@config/redis';
 import { queueConfig } from '@config/queue';
+import { ConsoleLog } from '@app/Helpers/ConsoleLog';
 /**
  * Init all bull queue config form queueConfig
  */
@@ -11,6 +12,13 @@ export class Manager {
         for (const queue of queueNames) {
             const connection = queueConfig[queue];
             const config = redisConfig[connection];
+            if (!config.host.length) {
+                ConsoleLog.debug(
+                    'Redis configuration is missing. Please check in .env!',
+                    false,
+                );
+                continue;
+            }
             const instance = new Queue(queue, `redis://${config.host}`, {
                 redis: { db: config.db as any },
             });
