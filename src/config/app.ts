dd65e0a5db718c597c72fb9e5d92d79f8032cd22
thutqv@fireapps.vn <admin23@24>
2023-05-18 13:19:22 +0700

    update config

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,7 +1,10 @@
+import { APP_ENVS } from '@constant/app';
+
 export const appConfig = {
-    env: process.env.APP_ENV,
+    env: process.env.APP_ENV ?? APP_ENVS.LOCAL,
+    port: process.env.PORT ?? 3000,
     lang: ['en'],
     providers: ['AppServiceProvider'],
-    host: process.env.HOST,
-    webhook_host: process.env.WEBHOOKS_HOST,
+    host: process.env.HOST ?? 'http://localhost',
+    webhook_host: process.env.WEBHOOKS_HOST ?? 'http://localhost',
 };
