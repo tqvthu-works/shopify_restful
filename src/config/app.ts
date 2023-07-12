031bddc07590d7959b08079841d34e3a2a073328
thutqv <thutqv@fireapps.vn>
2023-07-12 22:02:10 +0700

    Update appConfig

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,7 +2,7 @@ import { APP_ENVS } from '@constant/app';
 
 export const appConfig = {
     env: process.env.APP_ENV ?? APP_ENVS.LOCAL,
-    port: process.env.PORT ?? 3000,
+    port: process.env.APP_PORT ?? 3000,
     lang: ['en'],
     providers: ['AppServiceProvider'],
     host: process.env.HOST ?? 'http://localhost',
