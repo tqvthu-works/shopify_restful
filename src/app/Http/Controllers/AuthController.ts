261c257812f0252705ed2d8be028f635f485b107
thutqv <thutqv@fireapps.vn>
2023-08-24 22:59:54 +0700

    Demo Home page

--- src/app/Http/Controllers/AuthController.ts
+++ src/app/Http/Controllers/AuthController.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'inversify';
 import { AuthService } from '@app/Services/AuthService';
 import { Request, Response } from 'express';
 import { ApiController } from './ApiController';
+import { shopifyConfig } from '@config/shopify';
 
 @injectable()
 export class AuthController extends ApiController {
@@ -14,7 +15,11 @@ export class AuthController extends ApiController {
     public async install(request: Request, response: Response): Promise<Response> {
         return this.response(response, await this.service.buildInstallUrl(request));
     }
-    public async authCallback(request: Request, response: Response): Promise<Response> {
-        return this.response(response, await this.service.handleAuthCallback(request));
+    public async authCallback(request: Request, response: Response): Promise<void> {
+        await this.service.handleAuthCallback(request)
+        /* change to your url */
+         const tmpUrl = `https://admin.shopify.com/store/workshop-nfs/apps/${shopifyConfig.api_key}`;
+
+        return response.redirect(tmpUrl);
     }
 }
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,9 +4,19 @@ import ShopRouter from '@routes/api/shop';
 import WebhookRouter from '@routes/api/webhook';
 
 const ApiRouter = Express.Router();
-
+ApiRouter.use('/home', (req, res, next) => {
+  res.send(`
+  <html>
+      <body>
+          <h1>Nhậu với khầy chớ anh em! </h1>
+          <img src="https://t.vietgiaitri.com/2022/4/1/bien-cang-nam-idol-dinh-dam-bi-nghi-ngo-gian-lan-thi-cu-do-cap-3-nho-dac-quyen-ngoi-sao-379-6385563.jpeg" alt="Sample Image">
+      </body>
+  </html>
+`);
+});
 ApiRouter.use(AuthRouter);
 ApiRouter.use('/shops', ShopRouter);
 ApiRouter.use('/webhooks', WebhookRouter);
+
 
 export { ApiRouter };
